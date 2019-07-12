c238c363a879288a1316be4a4c50e7e12371aeca
Alex Noble <Swinkid@googlemail.com>
2019-07-12 18:17:00 +0100

    Added functionality to load/save nodes' public and private key.

--- managers/identityManager.js
+++ managers/identityManager.js
@@ -1,6 +1,12 @@
 const ECKey = require('ec-key');
 const fs = require('fs');
 
+const PUBLIC_KEY = "./public.pem";
+const PRIVATE_KEY = "./private.pem";
+
+let privateKey = null;
+let publicKey = null;
+
 /**
  *
  * @param app
@@ -9,23 +15,66 @@ const fs = require('fs');
  */
 const IdentityManager = (app, blockchain) => {
 
-	console.log(blockchain.isInitialized());
-	blockchain.initialize(null);
-	console.log(blockchain.isInitialized());
+	//TODO
+//	console.log(blockchain.isInitialized());
+//	blockchain.initialize(null);
+//	console.log(blockchain.isInitialized());
 
-	let randomKey = ECKey.createECKey('P-256');
-	console.log(randomKey.asPublicECKey().toString('pem'))
+	initializeKeys();
 
-	console.log(app.test)
 
 };
+
+/**
+ *
+ */
+function initializeKeys(){
+	if(keysExist()){
+		let privatePem = fs.readFileSync(PRIVATE_KEY);
+		let publicPem = fs.readFileSync(PUBLIC_KEY);
+
+		privateKey = new ECKey(privatePem, 'pem');
+		publicKey = new ECKey(publicPem, 'pem');
+
+		console.log(privateKey.toString('pem'));
+		console.log(publicKey.toString('pem'));
+
+	} else {
+		let randomKey = ECKey.createECKey('P-256');
+
+		publicKey = randomKey.asPublicECKey();
+		privateKey = randomKey;
+
+		saveKey(publicKey.toString('pem'), PUBLIC_KEY);
+		saveKey(privateKey.toString('pem'), PRIVATE_KEY)
+	}
+}
 
 /**
  * Checks to see if the node has a key pair already saved.
  * @returns {boolean}
  */
 function keysExist(){
-	return false;
+	try {
+		return fs.existsSync(PRIVATE_KEY) && fs.existsSync(PUBLIC_KEY);
+	} catch (e) {
+		return false;
+	}
+}
+
+/**
+ *
+ * @param key
+ * @param filename
+ */
+function saveKey(key, filename){
+	fs.writeFileSync(filename, key, function (error) {
+		if(error){
+			console.error(`Problem saving ${filename}`);
+		}
+
+		console.log(`Key ${filename} saved`);
+	});
 }
 
 module.exports = IdentityManager;
