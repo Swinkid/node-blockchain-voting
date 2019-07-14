8d4075a2f14371e4e453817c9b13d2abd769f274
Alex Noble <Swinkid@googlemail.com>
2019-07-14 21:05:40 +0100

    qr code test

--- models/block.js
+++ models/block.js
@@ -4,21 +4,53 @@ const Transaction = require('./transaction');
 class Block {
 
 	constructor(previousHash, data){
-		this.previousHash = previousHash;
-		this.data = data;
-		this.timeStamp = new Date();
-		this.hash = this.calculateHash();
+		this._timeStamp = new Date();
+		this._hash = this.calculateHash();
+		this._previousHash = previousHash;
+		this._data = data;
 	}
 
 	calculateHash(){
 		return StringUtils.encodeSha256(
-			this.previousHash +
-			this.data +
-			this.timeStamp +
+			this._previousHash +
+			this._data +
+			this._timeStamp +
 			this.nonce || 0
 		)
 	}
 
+
+	get previousHash() {
+		return this._previousHash;
+	}
+
+	set previousHash(value) {
+		this._previousHash = value;
+	}
+
+	get data() {
+		return this._data;
+	}
+
+	set data(value) {
+		this._data = value;
+	}
+
+	get timeStamp() {
+		return this._timeStamp;
+	}
+
+	set timeStamp(value) {
+		this._timeStamp = value;
+	}
+
+	get hash() {
+		return this._hash;
+	}
+
+	set hash(value) {
+		this._hash = value;
+	}
 }
 
 module.exports = Block;
