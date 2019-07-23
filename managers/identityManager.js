d2607b08fb3da9478d93d7efe316c37802bcb40c
Alex Noble <Swinkid@googlemail.com>
2019-07-23 16:15:28 +0100

    Bugfixes

--- managers/identityManager.js
+++ managers/identityManager.js
@@ -48,7 +48,7 @@ class IdentityManager {
 	 * @param filename
 	 */
 	saveKey(key, filename){
-		fs.writeFileSync(__basedir + '/' + filename, key,function (error) {
+		fs.writeFileSync(filename, key,function (error) {
 			if(error){
 				console.error(`Problem saving ${filename}`);
 			}
