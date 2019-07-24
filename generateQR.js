d9511433fb3e1fac03b8a047d56f6b21bc940bdd
Alex Noble <Swinkid@googlemail.com>
2019-07-24 17:02:52 +0100

    Bugfixes

--- generateQR.js
+++ generateQR.js
@@ -1,9 +1,5 @@
 const QRCode = require('qrcode');
-const ECKey = require('ec-key');
-
-
-
 
 process.on('message', message => {
-	QRCode.toFileStream(`${message.base}/node_keys/${message.index}.png`, message.key, function (err) {});
+	QRCode.toFile(`${message.base}/node_keys/${message.index}.png`, message.key, function (err) {});
 });
